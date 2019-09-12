bc177312c359dd750c1f8241beb22855ff0705cb
Patrick Kennedy <pkennedytx1@gmail.com>
2019-09-12 10:51:11 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(nameArray)
+    badgeArray = []
+    nameArray.each do | name |
+        message = badge_maker(name)
+        badgeArray.push(message)
+    end
+    return badgeArray
+end
+
+def assign_rooms(speakersArray)
+    speakerRoomArray =[]
+    speakersArray.each_with_index do | speaker, index |
+        speakerRoomArray.push("Hello, #{speaker}! You'll be assigned to room #{index+1}!")
+    end
+    return speakerRoomArray
+end
+
+def printer(nameArray)
+    batch_badge_creator(nameArray).each do | name |
+        puts name
+    end
+    assign_rooms(nameArray).each do | speaker |
+        puts speaker
+    end
+end
